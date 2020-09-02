3fe814c875dedcdba5d069bc7af48e4827e49a57
seepine <seepine@163.com>
2020-09-02 15:06:14 +0800

    去掉无用代码

--- src/main/java/com/seepine/auth/config/InterceptorConfig.java
+++ src/main/java/com/seepine/auth/config/InterceptorConfig.java
@@ -1,16 +1,11 @@
 package com.seepine.auth.config;
 
-import com.seepine.auth.config.AuthAutoConfigurer;
-import com.seepine.auth.entity.AuthProperties;
 import com.seepine.auth.interceptor.AuthenticationInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
